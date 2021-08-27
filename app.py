8f86c108eab5f514670fb6938513a8d563afe418
Khxxmz <87839821+Khxxmz@users.noreply.github.com>
2021-08-27 09:46:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,16 @@ camera = cv2.VideoCapture('rtsp://freja.hiof.no:1935/rtplive/_definst_/hessdalen
 
 app = Flask(__name__)
 
-def gen_frames(): # generate frame by frame from camera
+def gen_frames():  
     while True:
-          # Capture frame-by-frame
-          success, frame = camera.read() # read the camera frame
-          if not success:
-             break
-          else:
-             ret, buffer = cv2.imencode('.jpg', frame)
-             frame = buffer.tobytes()
-             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') # concat frame one by one and show result
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 data = [
         {
